0559dde1e71f88dccb0be2e70e05dfe601c1efe3
oljung <patrik_osterljung@hotmail.com>
2021-11-21 20:37:41 +0000

    feat: create repo list for github page

--- assets/scripts/github-information.js
+++ assets/scripts/github-information.js
@@ -12,9 +12,32 @@ const userInformationHTML = user => {
                     <img src ="${user.avatar_url}" width="80px" height="80px" alt ="${user.login}"> 
                 </a>
             </div>
-            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+            <p>Followers: ${user.followers} - Following: ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`
 }
+
+const repoInformationHTML = repos => {
+    if(repos.lenght === 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    let listItemsHTML = repos.map(repo => {
+        return `<li>
+                    <a href="${repo.html_url}" traget="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+}
+
+
 
 const fetchGitHubInformation = event => {
     let username = $("#gh-username").val();
@@ -29,10 +52,15 @@ const fetchGitHubInformation = event => {
     </div>`)
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        response => {
-            $('#gh-user-data').html(userInformationHTML(response));
+        (firstResponse, secondResponse) => {
+            let userData = firstResponse[0];
+            let repoData = secondResponse[0];
+
+            $('#gh-user-data').html(userInformationHTML(userData));
+            $('#gh-repo-data').html(repoInformationHTML(repoData));
         }, errorResponse => {
             if(errorResponse.status === 404) {
                 $('#gh-user-data').html(`<h2>No info found for user ${username}</h2>`);
